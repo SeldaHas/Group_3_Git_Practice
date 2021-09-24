a2fee20cc784b0732bcc3b9095214832093ab74c
Selda <seldahas.sh@gmail.com>
2021-09-24 13:42:06 +0300

    Selda branch commit

---
+++ src/test/java/com/gitPractice/test/SeldaTest.java
@@ -0,0 +1,8 @@
+package com.gitPractice.test;
+
+public class SeldaTest {
+
+    public static void main(String[] args) {
+        System.out.println("My nice work at my branch");
+    }
+}
---
+++ src/test/java/com/gitPractice/test/Test1.java
@@ -0,0 +1,5 @@
+package com.gitPractice.test;
+
+public class Test1 {
+
+}
